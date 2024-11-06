edcdbb342ab0e467db584d9973de59258939056d
smartin98 <s98amartin@gmail.com>
2024-11-06 12:17:37 -0800

    update rmse

--- .ipynb_checkpoints/calculate_rmse-checkpoint.py
+++ .ipynb_checkpoints/calculate_rmse-checkpoint.py
@@ -16,13 +16,15 @@ parser.add_argument('--start', type = str, help = 'start date for stats calculat
 parser.add_argument('--end', type = str, help = 'end date for stats calculation')
 parser.add_argument('--lon_bin_size', type = float, help = 'size of longitude bins')
 parser.add_argument('--lat_bin_size', type = float, help = 'size of latitude bins')
+parser.add_argument('--time_bin_size', type = int, help = 'size of time bins in days')
 parser.add_argument('--lon_min', type = float, help = 'minimum longitude [0-360] for calculating stats on subdomain')
 parser.add_argument('--lon_max', type = float, help = 'maximum longitude [0-360] for calculating stats on subdomain')
 parser.add_argument('--lat_min', type = float, help = 'minimum latitude for calculating stats on subdomain')
 parser.add_argument('--lat_max', type = float, help = 'maximum latitude for calculating stats on subdomain')
 parser.add_argument('--swot_dir', type = str, help = 'path to directory containing SWOT L3 data')
 parser.add_argument('--map_dir', type = str, help = 'path to directory containing mapped L4 data')
-
+parser.add_argument('--output_dir', type = str, help = 'path to directory to save results')
+parser.add_argument('--output_name', type = str, help = 'filename in which to save results')
 
 args = parser.parse_args()
 
@@ -64,6 +66,12 @@ if args.lat_bin_size is None:
 else:
     lat_bin_size = args.lat_bin_size
     
+if args.time_bin_size is None:
+    print('time_bin_size not specified, defaulting to 1 day...')
+    time_bin_size = 1
+else:
+    time_bin_size = args.time_bin_size
+    
 if args.swot_dir is None:
     print('swot_dir not specified, defaulting to /dat1/smart1n/SWOT/data/SWOT_L3_LR_SSH_EXPERT_1.0.2/')
     swot_dir = '/dat1/smart1n/SWOT/data/SWOT_L3_LR_SSH_EXPERT_1.0.2/'
@@ -75,13 +83,34 @@ if args.map_dir is None:
     map_dir = '/dat1/smart1n/NeurOST_SSH-SST/'
 else:
     map_dir = args.map_dir
+    
+if args.output_dir is None:
+    print('output_dir not specified, defaulting to ./results/')
+    output_dir = './results/'
+else:
+    output_dir = args.output_dir
+    if output_dir[-1] != '/':
+        output_dir = output_dir + '/'
+
+if args.output_name is None:
+    n_outputs = len(os.listdir(output_dir))
+    print('output_name not specified, defaulting to ' + f'rmse_ssh{n_outputs}.nc')
+    output_name = f'rmse_ssh{n_outputs}.nc'
+else:
+    output_name = args.output_name
+    if '.nc' not in output_name:
+        output_name = output_name + '.nc'
+
+
+output_path = output_dir + output_name
 
 
 start_dt = np.datetime64(start_date)
 end_dt = np.datetime64(end_date)
 
-num_days = (end_dt - start_dt).astype('timedelta64[D]') + 1
-date_array = np.arange(start_dt, end_dt + np.timedelta64(1, 'D')).astype('datetime64[s]')
+num_days = (((end_dt - start_dt)//time_bin_size).astype('timedelta64[D]') + 1).astype('int')
+
+date_array = np.array([(start_dt + np.timedelta64(n * time_bin_size, 'D')).astype('datetime64[s]') for n in range(num_days)]) #np.arange(start_dt, end_dt + np.timedelta64(1, 'D')).astype('datetime64[s]')
 
 N_lon = int((lon_bounds[1] - lon_bounds[0]) / lon_bin_size) + 1
 N_lat = int((lat_bounds[1] - lat_bounds[0]) / lat_bin_size) + 1
@@ -113,9 +142,10 @@ ds_results = xr.Dataset(
 )
 
 for i, t in enumerate(date_array):
-    print(f'Processing: {t}')
     start = t
-    end = t + np.timedelta64(1, 'D')
+    end = t + np.timedelta64(time_bin_size, 'D')
+
+    print(f'Processing: {start} to {end}')
     swot_data = SWOT_L3_Dataset(swot_dir, start, end, file_prefix = 'SWOT_L3_LR_SSH_Expert_XXX_YYY_')
     map_data = Map_L4_Dataset(map_dir, start, end)
     interp = interp_L4_to_L3(map_data, swot_data)
@@ -142,5 +172,5 @@ for i, t in enumerate(date_array):
         for var in stats.data_vars:
             ds_results[var][i,:,:] = stats[var].values
         
-ds_results.to_netcdf('./results/rmse.nc')
+ds_results.to_netcdf(output_path)
         
